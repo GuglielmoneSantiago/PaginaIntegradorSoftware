36e8eabfc6f5462c7a0501b12af02b3055957417
Lucas8276 <lucasnahuquin@gmail.com>
2024-06-09 22:34:19 -0300

    realizacion de los graficos!!!

--- backend/bck.py
+++ backend/bck.py
@@ -91,89 +91,229 @@ async def get_final_encuesta():
         content = f.read()
     return HTMLResponse(content=content)
 
-
-def obtener_datos_encuestas():
+#Pregunta 1
+def obtener_datos_pregunta1():
     cursor = collection.find({})
-    categorias = ['chocolate', 'atraccion', 'expectativa']
-    datos_encuestas = {categoria: [] for categoria in categorias}
-
+    respuestas = {"Esponjoso": 0, "Crujiente": 0, "Blando": 0, "Duro": 0}
     for doc in cursor:
-        for categoria in categorias:
-            if categoria in doc:
-                datos_encuestas[categoria].append(doc[categoria])
+        if "textura" in doc:
+            respuesta = doc["textura"]
+            if respuesta in respuestas:
+                respuestas[respuesta] += 1
+    return respuestas
 
-    return categorias, datos_encuestas
-
-def generar_grafico_telarana(categorias, valores):
-    fig, ax = plt.subplots(figsize=(10, 6), subplot_kw=dict(polar=True))
     
-    # Calcular los valores promedio y agregar el primer valor al final para cerrar el gráfico
-    valores_promedio = [sum(valores[i]) / len(valores[i]) if len(valores[i]) > 0 else 0 for i in range(len(categorias))]
-    valores_promedio.append(valores_promedio[0])  # Cerrar el gráfico
+@app.get("/graph/pregunta1", response_class=JSONResponse)
+async def get_graph_pregunta1():
+    respuestas = obtener_datos_pregunta1()
+    etiquetas = list(respuestas.keys())
+    imagen_barras = generar_grafico_barras(respuestas,etiquetas)
     
-    # Ajustar los ángulos de las categorías
-    angulos = [n / float(len(categorias)) * 2 * pi for n in range(len(categorias))]
-    angulos.append(angulos[0])
+    with tempfile.NamedTemporaryFile(suffix=".png", delete=False) as tmpfile:
+        imagen_barras.savefig(tmpfile.name, format="png")
 
-    ax.fill(angulos, valores_promedio, alpha=0.25, color='b')
-    ax.plot(angulos, valores_promedio, color='b')
+    with open(tmpfile.name, "rb") as image_file:
+        imagen_base64_barra_pregunta_uno = base64.b64encode(image_file.read()).decode("utf-8")
 
-    ax.set_theta_offset(pi / 2)
-    ax.set_theta_direction(-1)
+    return {"imagen_base64_barra_pregunta_uno": imagen_base64_barra_pregunta_uno}
 
-    ax.set_thetagrids([i * (360 / len(categorias)) for i in range(len(categorias))], categorias)
-    ax.set_ylim(0, 5)
+def generar_grafico_barras(respuestas,etiquetas):
+    fig, ax = plt.subplots()
+    ax.bar(etiquetas, respuestas.values())
+    ax.set_xlabel('Respuestas')
+    ax.set_ylabel('Cantidad')
+    ax.set_title('Respuestas a la Pregunta 1')
+    plt.xticks(rotation=45)
+    plt.tight_layout()
+    return fig
+#termina pregunta 1 
 
-    ax.set_title('Telaraña de Resultados de la Encuesta', size=20, color='black', y=1.1,pad=1)
+#Comienza pregunta 2 
+def obtener_datos_pregunta2():
+    cursor = collection.find({})
+    respuestas = {"Demasiado": 0, "Mucho": 0, "Poco": 0, "Nada": 0}
+    for doc in cursor:
+        if "consistencia" in doc:
+            respuesta = doc["consistencia"]
+            if respuesta in respuestas:
+                respuestas[respuesta] += 1
+    return respuestas
+
+@app.get("/graph/pregunta2", response_class=JSONResponse)
+async def get_graph_pregunta1():
+    respuestas = obtener_datos_pregunta2()
+    etiquetas = list(respuestas.keys())
+    imagen_barras = generar_grafico_barras_dos(respuestas,etiquetas)
     
+    with tempfile.NamedTemporaryFile(suffix=".png", delete=False) as tmpfile:
+        imagen_barras.savefig(tmpfile.name, format="png")
+
+    with open(tmpfile.name, "rb") as image_file:
+        imagen_base64_barra_pregunta_dos = base64.b64encode(image_file.read()).decode("utf-8")
+
+    return {"imagen_base64_barra_pregunta_dos": imagen_base64_barra_pregunta_dos}
+
+def generar_grafico_barras_dos(respuestas,etiquetas):
+    fig, ax = plt.subplots()
+    ax.bar(etiquetas, respuestas.values())
+    ax.set_xlabel('Respuestas')
+    ax.set_ylabel('Cantidad')
+    ax.set_title('Respuestas a la Pregunta 2')
+    plt.xticks(rotation=45)
+    plt.tight_layout()
+    return fig
+#Termina pregunta 2
+
+#Pregunta 3 
+def obtener_datos_chocolate():
+    cursor = collection.find({})
+    total_respuestas = 0
+    respuestas_chocolate = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0}
+    for doc in cursor:
+        if "chocolate" in doc:
+            total_respuestas += 1
+            respuesta = doc["chocolate"]
+            if respuesta in respuestas_chocolate:
+                respuestas_chocolate[respuesta] += 1
+    
+    # Calcular los porcentajes
+    porcentajes_chocolate = {str(key): (value / total_respuestas) * 100 for key, value in respuestas_chocolate.items()}
+    
+    return porcentajes_chocolate
+
+def generar_grafico_torta_chocolate(porcentajes_chocolate):
+    fig, ax = plt.subplots()
+    ax.pie(porcentajes_chocolate.values(), labels=porcentajes_chocolate.keys(), autopct='%1.1f%%', startangle=90)
+    ax.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+    ax.set_title('Valoración del Chocolate')
     return fig
 
- 
-@app.get("/graph", response_class=JSONResponse)
-async def get_graph():
-    categorias, datos_encuestas = obtener_datos_encuestas()
-    valores = [datos_encuestas[categoria] for categoria in categorias]
-
-    imagen_telarana = generar_grafico_telarana(categorias, valores)
+@app.get("/graph/chocolate", response_class=JSONResponse)
+async def get_graph_chocolate():
+    porcentajes_chocolate = obtener_datos_chocolate()
+    imagen_torta_chocolate = generar_grafico_torta_chocolate(porcentajes_chocolate)
     
     with tempfile.NamedTemporaryFile(suffix=".png", delete=False) as tmpfile:
-        imagen_telarana.savefig(tmpfile.name, format="png")
+        imagen_torta_chocolate.savefig(tmpfile.name, format="png")
 
     with open(tmpfile.name, "rb") as image_file:
-        imagen_base64 = base64.b64encode(image_file.read()).decode("utf-8")
+        imagen_base64_chocolate = base64.b64encode(image_file.read()).decode("utf-8")
 
-    return {"imagen_base64": imagen_base64}
+    return {"imagen_base64_chocolate": imagen_base64_chocolate}
+#finaliza pregunta 3
 
-@app.get("/graph/view", response_class=HTMLResponse)
-async def view_graph():
-    categorias, datos_encuestas = obtener_datos_encuestas()
-    valores = [datos_encuestas[categoria] for categoria in categorias]
+#Pregunta 4 
+def obtener_datos_atraccion():
+    cursor = collection.find({})
+    total_respuestas = 0
+    respuestas_atraccion = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0}
+    for doc in cursor:
+        if "chocolate" in doc:
+            total_respuestas += 1
+            respuesta = doc["chocolate"]
+            if respuesta in respuestas_atraccion:
+                respuestas_atraccion[respuesta] += 1
+    
+    # Calcular los porcentajes
+    porcentajes_atraccion = {str(key): (value / total_respuestas) * 100 for key, value in respuestas_atraccion.items()}
+    
+    return porcentajes_atraccion
 
-    imagen_telarana = generar_grafico_telarana(categorias, valores)
+def generar_grafico_torta_atraccion(porcentajes_atraccion):
+    fig, ax = plt.subplots()
+    ax.pie(porcentajes_atraccion.values(), labels=porcentajes_atraccion.keys(), autopct='%1.1f%%', startangle=90)
+    ax.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+    ax.set_title('Valoración de la atraccion')
+    return fig
+
+@app.get("/graph/atraccion", response_class=JSONResponse)
+async def get_graph_atraccion():
+    porcentajes_atraccion = obtener_datos_atraccion()
+    imagen_torta_atraccion = generar_grafico_torta_atraccion(porcentajes_atraccion)
     
     with tempfile.NamedTemporaryFile(suffix=".png", delete=False) as tmpfile:
-        imagen_telarana.savefig(tmpfile.name, format="png")
+        imagen_torta_atraccion.savefig(tmpfile.name, format="png")
 
     with open(tmpfile.name, "rb") as image_file:
-        imagen_base64 = base64.b64encode(image_file.read()).decode("utf-8")
+        imagen_base64_atraccion = base64.b64encode(image_file.read()).decode("utf-8")
 
-    # Construir la respuesta HTML con la imagen y la redirección
-    html_content = f'''
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="refresh" content="5; url=http://127.0.0.1:5500/src/HTML/paginarResultados.html">
-        <title>Redirecting...</title>
-    </head>
-    <body>
-        <img src="data:image/png;base64,{imagen_base64}" alt="Grafico de Telaraña">
-    </body>
-    </html>
-    '''
+    return {"imagen_base64_atraccion": imagen_base64_atraccion}
+#finaliza pregunta 4
 
-    return HTMLResponse(content=html_content, headers={"Content-Type": "text/html; charset=utf-8"})
+#Pregunta 5 
+def obtener_datos_expectativa():
+    cursor = collection.find({})
+    total_respuestas = 0
+    respuestas_expectativa = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0}
+    for doc in cursor:
+        if "expectativa" in doc:
+            total_respuestas += 1
+            respuesta = doc["expectativa"]
+            if respuesta in respuestas_expectativa:
+                respuestas_expectativa[respuesta] += 1
+    
+    # Calcular los porcentajes
+    porcentajes_expectativa = {str(key): (value / total_respuestas) * 100 for key, value in respuestas_expectativa.items()}
+    
+    return porcentajes_expectativa
 
+def generar_grafico_torta_expectativa(porcentajes_expectativa):
+    fig, ax = plt.subplots()
+    ax.pie(porcentajes_expectativa.values(), labels=porcentajes_expectativa.keys(), autopct='%1.1f%%', startangle=90)
+    ax.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+    ax.set_title('Valoración de las Expectativas')
+    return fig
+
+@app.get("/graph/expectativa", response_class=JSONResponse)
+async def get_graph_expectativa():
+    porcentajes_expectativa = obtener_datos_expectativa()
+    imagen_torta_expectativa = generar_grafico_torta_expectativa(porcentajes_expectativa)
+    
+    with tempfile.NamedTemporaryFile(suffix=".png", delete=False) as tmpfile:
+        imagen_torta_expectativa.savefig(tmpfile.name, format="png")
+
+    with open(tmpfile.name, "rb") as image_file:
+        imagen_base64_expectativa = base64.b64encode(image_file.read()).decode("utf-8")
+
+    return {"imagen_base64_expectativa": imagen_base64_expectativa}
+#finaliza pregunta 5
+
+#Pregunta 7
+def obtener_datos_pregunta7():
+    cursor = collection.find({})
+    respuestas = {"Dulce": 0, "Amargo": 0, "Salado": 0, "Acido": 0}
+    for doc in cursor:
+        if "sabores" in doc:
+            respuesta = doc["sabores"]
+            if respuesta in respuestas:
+                respuestas[respuesta] += 1
+    return respuestas
+
+    
+@app.get("/graph/sabores", response_class=JSONResponse)
+async def get_graph_pregunta7():
+    respuestas = obtener_datos_pregunta7()
+    etiquetas = list(respuestas.keys())
+    imagen_barras = generar_grafico_barras_sabores(respuestas,etiquetas)
+    
+    with tempfile.NamedTemporaryFile(suffix=".png", delete=False) as tmpfile:
+        imagen_barras.savefig(tmpfile.name, format="png")
+
+    with open(tmpfile.name, "rb") as image_file:
+        imagen_base64_barra_pregunta_siete = base64.b64encode(image_file.read()).decode("utf-8")
+
+    return {"imagen_base64_barra_pregunta_siete": imagen_base64_barra_pregunta_siete}
+
+def generar_grafico_barras_sabores(respuestas,etiquetas):
+    fig, ax = plt.subplots()
+    ax.bar(etiquetas, respuestas.values())
+    ax.set_xlabel('Respuestas')
+    ax.set_ylabel('Cantidad')
+    ax.set_title('Respuestas a la Pregunta 7')
+    plt.xticks(rotation=45)
+    plt.tight_layout()
+    return fig
+#Finaliza pregunta 7
 @app.get("/ver_grafico", response_class=HTMLResponse)
 async def get_form():
     with open(pagina_Resultado, encoding="utf-8") as f:
