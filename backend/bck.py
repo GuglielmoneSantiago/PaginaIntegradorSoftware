db13da82d401458bd9892eb18072a384a39d2000
Lucas8276 <lucasnahuquin@gmail.com>
2024-06-12 19:23:03 -0300

    graficoMaRRON

--- backend/bck.py
+++ backend/bck.py
@@ -5,6 +5,7 @@ from fastapi import FastAPI, Form, HTTPException, Request,Response
 from fastapi.responses import FileResponse, HTMLResponse, RedirectResponse, StreamingResponse
 from fastapi.staticfiles import StaticFiles
 from matplotlib import pyplot as plt
+import matplotlib.pyplot as plt
 from numpy import pi
 from pymongo import MongoClient
 from pydantic import BaseModel
@@ -183,10 +184,19 @@ def obtener_datos_chocolate():
     return porcentajes_chocolate
 
 def generar_grafico_torta_chocolate(porcentajes_chocolate):
+    # Crear la figura y los ejes
     fig, ax = plt.subplots()
-    ax.pie(porcentajes_chocolate.values(), labels=porcentajes_chocolate.keys(), autopct='%1.1f%%', startangle=90)
-    ax.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+
+    # Definir los colores en tonalidades de marrón
+    colores_marron = ['#D2B48C', '#A0522D', '#8B4513', '#563527', '#800000']
+
+    # Dibujar el gráfico de torta
+    ax.pie(porcentajes_chocolate.values(), labels=porcentajes_chocolate.keys(), autopct='%1.1f%%', startangle=90, colors=colores_marron)
+    ax.axis('equal')  # Asegurar que el gráfico se dibuje como un círculo
+
+    # Establecer el título
     ax.set_title('Valoración del Chocolate')
+
     return fig
 
 @app.get("/graph/chocolate", response_class=JSONResponse)
